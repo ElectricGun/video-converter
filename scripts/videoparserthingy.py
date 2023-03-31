fb4f57fe8562193c2eef3b3d04d6afcef0cc9136
ElectricGun <68138799+ElectricGun@users.noreply.github.com>
2023-04-01 00:25:32 +0700

    fixed bugs #2

--- scripts/videoparserthingy.py
+++ scripts/videoparserthingy.py
@@ -7,85 +7,125 @@ import math as Math
 outputDir = ""    #insert output dir
 media = ""    #insert file
 length = int(cv2.VideoCapture(media).get(cv2.CAP_PROP_FRAME_COUNT))
-resize = 100 / 100    #resize percentage
+#length = 200    #length override
+resize = 10 / 100    #resize percentage
 
 cpuThreads = mp.cpu_count()
-nprocesses = 12
+nprocesses = 1
 framesPerProcess = Math.ceil(length / nprocesses)     #rough frames per process amount
+treshold = 100000    #maximum array size per file
+
+def scale(height, width, scale):    #define skips as array to work with a non integer resize factor
+                            
+    newHeight, newWidth = Math.ceil(height * scale), Math.ceil(width * scale)
+    try:
+        skipH, skipW =  height / newHeight, width / newWidth
+    except ZeroDivisionError:
+        skipH, skipW = 1, 1
+
+    skipsHeight, skipsWidth = [], []
+    remainder0, remainder1 = 0, 0
+
+    for i in range(newHeight):
+        remainder0 += skipH - Math.floor(skipH)
+        tempH = Math.floor(skipH)
+        if remainder0 >= 1:
+            tempH += 1
+            remainder0 -= 1
+        skipsHeight.append(tempH)
+
+    for i in range(newWidth):
+        remainder1 += skipW - Math.floor(skipW)
+        tempW = Math.floor(skipW)
+        if remainder1 >= 1:
+            tempW += 1
+            remainder1 -= 1
+        skipsWidth.append(tempW) 
+
+    return newHeight, newWidth, skipsHeight, skipsWidth
+    
 
 def parseToFile(media, outputDir, batchLength, endFrame, processName):
 
     fileName = media.split("/")[-1]
-    outputFileName = os.path.join(outputDir, fileName+"_raw"+str(processName)+".txt")
-    outputFile = open(outputFileName, "w")
-
     startFrame = processName * framesPerProcess
+    
 
-    #print("Process:", processName, startFrame, endFrame, batchLength, "\n")
-    for currFrame in range(startFrame, endFrame):
-        outputFile.write("[")    #open array
-        
+    subBatchNumber = 0
+    stop = False
+    while True:
+        if stop == True: break
+
+        outputFileName = os.path.join(outputDir, fileName+"_raw"+str(processName)+"_"+str(subBatchNumber)+".json")
+        outputFile = open(outputFileName, "w")
+        outputFile.write("{\"seq\":\n")    #open json
+        outputFile.write("[")    #open batch array
+
         cap = cv2.VideoCapture(media)
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-        cap.set(1, currFrame)                                               
+        cap.set(1, 0)                                               
         res, frame = cap.read()
-        height, width, channels = frame.shape
-                                   
-        newHeight, newWidth = Math.ceil(height * resize), Math.ceil(width * resize)
-        try:
-            skipH, skipW =  height / newHeight, width / newWidth
-        except ZeroDivisionError:
-            skipH, skipW = 1, 1
-
-        skipsHeight, skipsWidth = [], []
-        remainder0, remainder1 = 0, 0
-
-    ###############################################     define skips as array to work with a non integer resize factor
-
-        for i in range(newHeight):
-            remainder0 += skipH - Math.floor(skipH)
-            tempH = Math.floor(skipH)
-            if remainder0 >= 1:
-                tempH += 1
-                remainder0 -= 1
-            skipsHeight.append(tempH)
-
-        for i in range(newWidth):
-            remainder1 += skipW - Math.floor(skipW)
-            tempW = Math.floor(skipW)
-            if remainder1 >= 1:
-                tempW += 1
-                remainder1 -= 1
-            skipsWidth.append(tempW)                                                                    
-            
-    ##############################################      parse and write current frame a file
-
-        output, outputY = [], []
-        pickerY = 0
-        for y in range(0, newHeight):
-            pickerX = 0
-            pickerY += skipsHeight[y]
-            if len(outputY) > 0:
-                output.append(list(outputY))
-                                                                     
-            outputY = []
-            for x in range(0, newWidth):
-                pickerX += skipsWidth[x]
-                try:
-                    bright = Math.floor(sum(frame[pickerY][pickerX]) / 255)
-
-                except IndexError:
-                    pass
-                outputY.append(str(bright))
-                                                
-        outputString = "],\n[".join(",".join(str(x[0]) for x in y) for y in output)
-        outputFile.write("["+outputString+"]")
-        outputFile.write("]\n")    #close array
+        height, width, channels = frame.shape    #get frame shape of first frame
         
-        if processName == 0:
-            print(str(((currFrame - startFrame) / (endFrame - startFrame) * 100))+"% complete"+"\n")           #print percentage
+        newHeight, newWidth, skipsHeight, skipsWidth = scale(height, width, resize)
+
+        totalSubBatchOutputLength = 0
+        for currFrame in range(startFrame, endFrame):
+            outputFile.write("[")    #open frame array
+            
+            cap = cv2.VideoCapture(media)
+            #fps = int(cap.get(cv2.CAP_PROP_FPS))
+            cap.set(1, currFrame)                                               
+            res, frame = cap.read()
+            try:
+                height, width, channels = frame.shape
+            except AttributeError:
+                stop = True; break                                                                   
+                
+        ##############################################      parse and write current frame a file
+
+            output, outputY = [], []
+            pickerY = 0
+            for y in range(0, newHeight):
+                pickerX = 0
+                pickerY += skipsHeight[y]
+                if len(outputY) > 0:
+                    output.append(list(outputY))
+                                                                        
+                outputY = []
+                for x in range(0, newWidth):
+                    pickerX += skipsWidth[x]
+                    try:
+                        bright = Math.floor(sum(frame   [pickerY][pickerX]) / 255)
+
+                    except IndexError:
+                        bright = 0
+                        pass
+
+                    outputY.append(str(bright))
+
+            outputLength = len(output) * len(output[0])          
+            outputString = "],\n[".join(",".join(str(x[0]) for x in y) for y in output)
+            outputFile.write("["+outputString+"]")
+            outputFile.write("],\n")    #close frame array
+
+            totalSubBatchOutputLength += outputLength
+            if processName == 0:
+                #print(str(((currFrame - startFrame) / (endFrame - startFrame) * 100))+"% complete"+"\n", totalSubBatchOutputLength)           #print percentage
+                pass
+            
+            if totalSubBatchOutputLength + outputLength > treshold:
+                subBatchNumber += 1
+                startFrame = currFrame + 1
+                print("Batch exceeded max treshold")
+                break
+
+            if currFrame >= endFrame - 1: stop = True
+                
+        outputFile.write("\"STOP\"]")    #close batch array
+        outputFile.write("\n}")    #close json
+        outputFile.close()
     print("Process", processName + 1, "complete")
-    outputFile.close()
+    print(totalSubBatchOutputLength, "Process:", processName, "\n")   
 
 
 
@@ -107,3 +147,15 @@ if __name__ == "__main__":
     for i in range(nprocesses):
         processes[i].start()
         print(processes[i].name, "started! \n")
+    
+    done = 0
+    while True:
+        if (done == 1): break
+        for i in range(nprocesses):
+            if (processes[i].is_alive() == False):
+                done = 1
+            else: 
+                done = 0
+                break
+    print("Complete")
+    #intitate finalisation
